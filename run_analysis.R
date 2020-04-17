bacd0800e0046301e8a0f9ad792fa0311fbe3ca3
MulhamJarjanazi <55259785+MulhamJarjanazi@users.noreply.github.com>
2020-04-17 12:41:44 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,15 @@
 
 Analyze_Dataset <- function(){
+##choosing the columns for mean and std
     selected_columns <- c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,
                           83,84,85,86,121,122,123,124,125,126,
                           201,202,214,215,227,228,240,241,253,
                           254,266,267,268,269,270,271,345,346,
                           347,348,349,350,424,425,426,427,428,
                           429,503,504,516,517,529,530,542,543)
-    
+
+##collecting data from all the required text files
+  
     xtrain <- read.table("train/X_train.txt", sep = "")
     ytrain <- read.table("train/y_train.txt", sep = "")
     train_subjects <- read.table("train/subject_train.txt", sep = "")
@@ -24,8 +27,9 @@ Analyze_Dataset <- function(){
     X <- rbind(xtrain, xtest)
     y <- rbind(ytrain, ytest)
     subjects <- rbind(train_subjects, test_subjects)
+ 
+##changing the values for $Activity from numbers to descriptive terms
     labels <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING")
-    
     names(subjects) <- c("Subject")
     names(y) <- c("Activity")
     names_x <- NULL
@@ -41,6 +45,7 @@ Analyze_Dataset <- function(){
     
     names(X) <- names_x
     
+##combining all columns to one big dataset that is returned
     FullDataSet <- cbind(subjects,X,y)
     
     for (j in 1:nrow(FullDataSet)){
